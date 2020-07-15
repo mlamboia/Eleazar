f9ba6ceb2430a9324836ea441624c288220d38c3
matheus.lamboia <matheuslamboia@matheus.lamboia>
2020-07-14 23:08:31 -0300

    server up

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/contact', { useNewUrlParser: true})
+  .connect('mongodb://127.0.0.1:27017/contact', { useNewUrlParser: true })
   .catch(error => {
     console.log('Connection error', error.message)
   })
